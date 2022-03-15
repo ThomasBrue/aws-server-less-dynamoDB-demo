17295c32530d7028b58a2bd8fd0dd42187d3c694
ThomasBrue <t.bruendl@gmail.com>
2022-03-15 22:14:41 +0100

    removed unnecessary code

--- handler.js
+++ handler.js
@@ -1,81 +1,15 @@
 "use strict";
 
-module.exports.hello = async (event, context, callback) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message:
-          "Go Serverless v1.0! Your function executed successfully! Yes Sir!",
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-};
-
-module.exports.userInfo = async (event, context, callback) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        name: "Jimmy",
-        age: "5",
-        city: "Paris",
-      },
-      null,
-      2
-    ),
-  };
-};
-
-module.exports.eatApple = async (event, context, callback) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "I like to eat apples",
-      },
-      null,
-      2
-    ),
-  };
-};
-
 const AWS = require("aws-sdk");
 const ddb = new AWS.DynamoDB.DocumentClient({ region: "us-east-1" });
 
-module.exports.writeMessage2 = async (event, context, callback) => {
-  console.log("WriteMessage2 was called....................................");
-  const requestId = context.awsRequestId;
+// ----UTILITY----------------------------------------------------------------------
 
-  await createMessage(requestId)
-    .then(() => {
-      callback(null, {
-        statusCode: 201,
-        body: "",
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-        },
-      });
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-};
-
-function createMessage(requestId) {
-  const params = {
-    TableName: "Message",
-    Item: {
-      messageId: requestId,
-      message: "Johny",
-      someValue: "hello dear",
-    },
+function response(statusCode, message) {
+  return {
+    statusCode: statusCode,
+    body: JSON.stringify(message),
   };
-
-  return ddb.put(params).promise();
 }
 
 // ----CREATE CUSTOMER---------------------------------------------------------------------------------
@@ -118,15 +52,6 @@ module.exports.createCustomer = async (event, context, callback) => {
       console.error(err);
     });
 };
-
-// ----UTILITY----------------------------------------------------------------------
-
-function response(statusCode, message) {
-  return {
-    statusCode: statusCode,
-    body: JSON.stringify(message),
-  };
-}
 
 //------GET ALL CUSTOMER--------------------------------------------------------
 
